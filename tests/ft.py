829f2a3a167d89c8dd9df109d850728bcbe86c70
Procinogen <mrcarrotman12345@gmail.com>
2019-05-05 05:59:10 -0400

    Added more to the training samples.

--- tests/ft.py
+++ tests/ft.py
@@ -1,7 +1,25 @@
 import fastText
+import bs4
+import requests
+from bs4 import BeautifulSoup
+import re
+from summa import summarizer
+
+def parse(url):
+    r = requests.get(url)
+    c = r.content
+    soup = BeautifulSoup(c, 'html.parser')
+
+    paragraph = str(soup.find_all('p'))
+    #x = "".join(paragraph)
+    for link in soup.find_all('a', attrs={'href': re.compile("^http://")}):
+        print (link.get('href'))
+    newlist = re.sub('<[^>]+>', '', paragraph)
+    return newlist[0]
 
 def main():
     model = fastText.train_supervised('..\Data\Gerard.txt')
-    print(model.predict("These tools collect information sent by your device or our Service, including the web pages you visit, add-ons, and other information that assists us in improving the Service."))
+    print(parse("https://www.example.com"))
+    print(model.predict(parse("https://www.example.com")))
 
 main()
